c9a07dd6a7d3c194091cb55d50ebc47184d5296c
KevinCifuentes <kevin.cifuentes@opendeusto.es>
2016-10-10 12:46:22 +0200

    Added the functionality related to the movement of the train, simulating a wake in the map

--- server.js
+++ server.js
@@ -4,15 +4,43 @@
 **/
 
 var http = require('http'),
+	Stomp = require('stomp-client'),
+	cluster = require('cluster'),
     url = require('url');
-	
+
+var destino = '/topic/jms.topic.test';
+var cliente;
+
 function start(dispatch, handlers)
 {
-	http.createServer(function(req, res) {
+	if(cluster.isMaster)
+	{
+		http.createServer(function(req, res) {
 		var _url = url.parse(req.url).pathname;
 		dispatch(handlers, _url, req, res);
-	}).listen(8888);
-	console.log("Server started !! ");
+		}).listen(8888);
+		console.log("Server started !! ");
+		cluster.fork();
+
+	}
+	else
+	{
+		client = new Stomp('130.206.138.15', 61613, '','');
+
+		client.connect(function(sessionId) {
+		    client.subscribe(destino, function(body, headers) {
+		      //Obtengo mensajes de la posicion de los trenes y almacenaría en la base de datos
+
+		    });
+
+		    //client.publish(destination, 'Oh herrow');
+		});
+	}
+	
+
+	
+
+	
 }
 
 exports.start = start;
--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,59 @@ var app = angular.module("giv2railapp", ['leaflet-directive'
 app.config(function($logProvider){
   $logProvider.debugEnabled(false);
 });
+
+var modalHistoricoAbierto = false;
+
 var numeroMarcadores = []; //cuando haya más de un tren
 var todosMarcadores =[];
 var numeroMarcadoresAhora = [];
 var ultimoActualizadoTrenes = [];
 var primeraVez = true;
+var numeroMaximoAMostrar = 5;
+var arrayIconos = ["img/icontrainb0.png", "img/icontrainb25.png", "img/icontrainb50.png", "img/icontrainb75.png", "img/icontrain.png"]
+
+function establecerEventos(){
+		// Coge el modal
+	var modal = document.getElementById('historicoModal');
+
+	// coje el boton de historico
+	var btn = document.getElementById("historico");
+
+	// coje el span del modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// Cuando se pulsa al historico se muestra el modal o se cierra si estaba abierto
+	btn.onclick = function(event) {
+		if(modalHistoricoAbierto === false)
+		{
+			modalHistoricoAbierto = true;
+			modal.style.display = "block";
+		}
+		else
+		{
+			modalHistoricoAbierto = false;
+			modal.style.display = "none";
+		}
+	    
+	}
+
+	//Cuando se pulse la x se cierra el modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	    modalHistoricoAbierto = false;
+	}
+}
 
 app.controller("giv2railController", [ '$scope', 'leafletData', function($scope, leafletData) {
+	establecerEventos();
+	 $scope.data = {
+	    opcionesBusqueda: [
+	      "ID tren",
+	      "Fecha"
+	    ],
+	    seleccion: "ID tren",
+	    inputBusqueda: ""
+    };
 	$scope.markers = new Array();
 	marcador = {
 	                lat: 43.046514,
@@ -22,14 +68,14 @@ app.controller("giv2railController", [ '$scope', 'leafletData', function($scope,
                         label: {
                             message: "El tren está aquí",
                             options: {
-                                noHide: true
+                                noHide: false
                             }
                         },
                         icon: {
-                        	iconUrl: 'img/icontrain.png',
+                        	iconUrl: arrayIconos[4],
 		                    iconSize:     [38, 38], // tamano del icono
 		                    iconAnchor:   [15, 38], // punto del icono que correponde a la localizacion del marcador
-		                    popupAnchor:  [-3, -76] // punto relativo a donde el popup debería abrirse
+		                    popupAnchor:  [-3, -76], // punto relativo a donde el popup debería abrirse
                         }
 	            };
 	numeroMarcadoresAhora["tren1"] = 1;
@@ -50,7 +96,7 @@ app.controller("giv2railController", [ '$scope', 'leafletData', function($scope,
 	           if(tipo === "posicion"){
 	           	 var contenido = JSON.parse(lineas[contador]);
 	           	 //	HABRÍA QUE OBTENER EL ID DE CADA TREN Y MIRAR SI YA HAY MARCADORES Y SI NO AÑADIR
-	           	 if(numeroMarcadoresAhora["tren1"] != 5)
+	           	 if(numeroMarcadoresAhora["tren1"] != numeroMaximoAMostrar)
 	           	 {
 	           	 	ultimoActualizadoTrenes["tren1"] = -1;
 	           	 	marcador = {
@@ -60,13 +106,13 @@ app.controller("giv2railController", [ '$scope', 'leafletData', function($scope,
                         title: "Tren",
                         draggable: true,
                         label: {
-                            message: "El tren está aquí" + numeroMarcadoresAhora["tren1"] ,
+                            message: "El tren está aquí" + numeroMarcadoresAhora["tren1"] +" "+ new Date().toUTCString(),
                             options: {
-                                noHide: true
+                                noHide: false
                             }
                         },
                         icon: {
-                        	iconUrl: 'img/icontrain.png',
+                        	iconUrl: arrayIconos[4],
 		                    iconSize:     [38, 38], // tamano del icono
 		                    iconAnchor:   [15, 38], // punto del icono que correponde a la localizacion del marcador
 		                    popupAnchor:  [-3, -76] // punto relativo a donde el popup debería abrirse
@@ -80,14 +126,44 @@ app.controller("giv2railController", [ '$scope', 'leafletData', function($scope,
 	           	 {
 	           	 	todosMarcadores["tren1"][ultimoActualizadoTrenes["tren1"]+1].lat = contenido.latitud;
 	           	 	todosMarcadores["tren1"][ultimoActualizadoTrenes["tren1"]+1].lng = contenido.longitud;
-	           	 	if(ultimoActualizadoTrenes["tren1"] === 3)
-	           	 	{
-	           	 		ultimoActualizadoTrenes["tren1"] = -1;
-	           	 	}
-	           	 	else
-	           	 	{
-	           	 		ultimoActualizadoTrenes["tren1"]++;
-	           	 	}
+	           	 	todosMarcadores["tren1"][ultimoActualizadoTrenes["tren1"]+1].icon.iconUrl = arrayIconos[4];
+	           	 	todosMarcadores["tren1"][ultimoActualizadoTrenes["tren1"]+1].label.message = "El tren está aquí "+ new Date().toUTCString();
+	           	 	switch(ultimoActualizadoTrenes["tren1"]+1) {
+					    case 0:
+					        todosMarcadores["tren1"][4].icon.iconUrl = arrayIconos[3];
+		           	 		todosMarcadores["tren1"][3].icon.iconUrl = arrayIconos[2];
+		           	 		todosMarcadores["tren1"][2].icon.iconUrl = arrayIconos[1];
+		           	 		todosMarcadores["tren1"][1].icon.iconUrl = arrayIconos[0];
+		           	 		ultimoActualizadoTrenes["tren1"]++;
+					        break;
+					    case 1:
+					    	todosMarcadores["tren1"][4].icon.iconUrl = arrayIconos[2];
+		           	 		todosMarcadores["tren1"][3].icon.iconUrl = arrayIconos[1];
+		           	 		todosMarcadores["tren1"][2].icon.iconUrl = arrayIconos[0];
+		           	 		todosMarcadores["tren1"][0].icon.iconUrl = arrayIconos[3];
+					        ultimoActualizadoTrenes["tren1"]++;
+					        break;
+					    case 2:
+					    	todosMarcadores["tren1"][4].icon.iconUrl = arrayIconos[1];
+		           	 		todosMarcadores["tren1"][3].icon.iconUrl = arrayIconos[0];
+		           	 		todosMarcadores["tren1"][1].icon.iconUrl = arrayIconos[3];
+		           	 		todosMarcadores["tren1"][0].icon.iconUrl = arrayIconos[2];
+					        ultimoActualizadoTrenes["tren1"]++;
+					        break;
+					    case 3:
+					        todosMarcadores["tren1"][2].icon.iconUrl = arrayIconos[3];
+		           	 		todosMarcadores["tren1"][1].icon.iconUrl = arrayIconos[2];
+		           	 		todosMarcadores["tren1"][0].icon.iconUrl = arrayIconos[1];
+		           	 		todosMarcadores["tren1"][4].icon.iconUrl = arrayIconos[0];
+		           	 		ultimoActualizadoTrenes["tren1"]++;
+					        break;
+					    case 4:
+					    	todosMarcadores["tren1"][3].icon.iconUrl = arrayIconos[3];
+		           	 		todosMarcadores["tren1"][2].icon.iconUrl = arrayIconos[2];
+		           	 		todosMarcadores["tren1"][1].icon.iconUrl = arrayIconos[1];
+		           	 		todosMarcadores["tren1"][0].icon.iconUrl = arrayIconos[0];
+		           	 		ultimoActualizadoTrenes["tren1"] = -1;
+					}
 	           	 }
 	           	 if(primeraVez === true)
 	           	 {
@@ -109,4 +185,9 @@ app.controller("giv2railController", [ '$scope', 'leafletData', function($scope,
             scrollWheelZoom: false
         }
     });
+
+    $scope.buscarHistorico = function(){
+    	console.log("Llego");
+    	console.log($scope.data.seleccion);
+    }
 }]);
